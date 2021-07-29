669db6934c1f153f93a645e019a980911fb9b150
yahyaaris <yahyanurmuhdarisharry@gmail.com>
2021-07-29 10:12:42 +0800

    Thursday Morning Update #1

--- resources/views/drone/index.blade.php
+++ resources/views/drone/index.blade.php
@@ -20,7 +20,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../../../assets/img/apple-icon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="#">
   <link rel="icon" type="image/png" href="../../../assets/img/favicon.png">
   <title>
     Soft UI Dashboard PRO by Creative Tim
@@ -71,9 +71,6 @@
               <div class="d-lg-flex">
                 <div>
                   <h5 class="mb-0">All Drones</h5>
-                  <p class="text-sm mb-0">
-                    A lightweight, extendable, dependency-free javascript HTML table plugin.
-                  </p>
                 </div>
                 <div class="ms-auto my-auto mt-lg-0 mt-4">
                   <div class="ms-auto my-auto">
@@ -93,10 +90,6 @@
                               <label class="custom-control-label" for="importCheck">I accept the terms and conditions</label>
                             </div>
                           </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn bg-gradient-secondary btn-sm" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn bg-gradient-primary btn-sm">Upload</button>
-                          </div>
                         </div>
                       </div>
                     </div>
@@ -104,175 +97,44 @@
                 </div>
               </div>
             </div>
-            <div class="card-body px-0 pb-0">
-              <div class="table-responsive">
-                <table class="table table-flush" id="products-list">
-                  <thead class="thead-light">
-                    <tr>
-                      <th>Drone</th>
-                      <th>Brand</th>
-                      <th>Model</th>
-                      <th>Year</th>
-                      <th>Quantity</th>
-                      <th>Status</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <div class="d-flex">
-                          <div class="form-check my-auto">
-                            <input class="form-check-input" type="checkbox" id="customCheck1" checked>
-                          </div>
-                          <img class="w-10 ms-3" src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/ecommerce/adidas-hoodie.jpg" alt="hoodie">
-                          <h6 class="ms-3 my-auto">Drone 1</h6>
-                        </div>
-                      </td>
-                      <td class="text-sm">Brand01</td>
-                      <td class="text-sm">#0001</td>
-                      <td class="text-sm">2020</td>
-                      <td class="text-sm">1</td>
-                      <td>
-                        <span class="badge badge-danger badge-sm">Repairing</span>
-                      </td>
-                      <td class="text-sm">
-                        <a href="javascript:;" data-bs-toggle="tooltip" data-bs-original-title="Preview profile">
-                          <i class="fas fa-eye text-secondary"></i>
-                        </a>
-                        <a href="javascript:;" class="mx-3" data-bs-toggle="tooltip" data-bs-original-title="Edit profile">
-                          <i class="fas fa-user-edit text-secondary"></i>
-                        </a>
-                        <a href="javascript:;" data-bs-toggle="tooltip" data-bs-original-title="Delete profile">
-                          <i class="fas fa-trash text-secondary"></i>
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-flex">
-                          <div class="form-check my-auto">
-                            <input class="form-check-input" type="checkbox" id="customCheck2" checked>
-                          </div>
-                          <img class="w-10 ms-3" src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/ecommerce/macbook-pro.jpg" alt="mac">
-                          <h6 class="ms-3 my-auto">Drone 02</h6>
-                        </div>
-                      </td>
-                      <td class="text-sm">Brand01</td>
-                      <td class="text-sm">#0002</td>
-                      <td class="text-sm">20181</td>
-                      <td class="text-sm">10</td>
-                      <td>
-                        <span class="badge badge-danger badge-sm">Repairing</span>
-                      </td>
-                      <td class="text-sm">
-                        <a href="javascript:;" data-bs-toggle="tooltip" data-bs-original-title="Preview profile">
-                          <i class="fas fa-eye text-secondary"></i>
-                        </a>
-                        <a href="javascript:;" class="mx-3" data-bs-toggle="tooltip" data-bs-original-title="Edit profile">
-                          <i class="fas fa-user-edit text-secondary"></i>
-                        </a>
-                        <a href="javascript:;" data-bs-toggle="tooltip" data-bs-original-title="Delete profile">
-                          <i class="fas fa-trash text-secondary"></i>
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="d-flex">
-                          <div class="form-check my-auto">
-                            <input class="form-check-input" type="checkbox" id="customCheck3">
-                          </div>
-                          <img class="w-10 ms-3" src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/ecommerce/metro-chair.jpg" alt="metro-chair">
-                          <h6 class="ms-3 my-auto">Drone 03</h6>
-                        </div>
-                      </td>
-                      <td class="text-sm">Brand 02</td>
-                      <td class="text-sm">#0003</td>
-                      <td class="text-sm">2005</td>
-                      <td class="text-sm">4</td>
-                      <td>
-                        <span class="badge badge-success badge-sm">Good Condition</span>
-                      </td>
-                      <td class="text-sm">
-                        <a href="javascript:;" data-bs-toggle="tooltip" data-bs-original-title="Preview profile">
-                          <i class="fas fa-eye text-secondary"></i>
-                        </a>
-                        <a href="javascript:;" class="mx-3" data-bs-toggle="tooltip" data-bs-original-title="Edit profile">
-                          <i class="fas fa-user-edit text-secondary"></i>
-                        </a>
-                        <a href="javascript:;" data-bs-toggle="tooltip" data-bs-original-title="Delete profile">
-                          <i class="fas fa-trash text-secondary"></i>
-                        </a>
-                      </td>
-                    </tr>
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th>Product</th>
-                      <th>Category</th>
-                      <th>Price</th>
-                      <th>SKU</th>
-                      <th>Quantity</th>
-                      <th>Status</th>
-                      <th>Action</th>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
+            <table class="table table-striped table table-bordered">
+              <tr>
+                  <td>DRONE</td>
+                  <td>BRAND</td>
+                  <td>MODEL</td>
+                  <td>YEAR</td>
+                  <td>QUANTITY</td>
+                  <td>STATUS</td>
+              </tr>
+              <tr>
+                  <td>Drone_01</td>
+                  <td>Brand_01</td>
+                  <td>#0001</td>
+                  <td>2020</td>
+                  <td>1</td>
+                  <td>Repairing</td>
+              </tr>
+              <tr>
+                <td>Drone_02</td>
+                <td>Brand_01</td>
+                <td>#0002</td>
+                <td>2018</td>
+                <td>10</td>
+                <td>Good Condition</td>
+            </tr>
+            <tr>
+              <td>Drone_03</td>
+              <td>Brand_02</td>
+              <td>#0003</td>
+              <td>2015</td>
+              <td>4</td>
+              <td>Good Condition</td>
+          </tr>
+          </table>
             </div>
           </div>
         </div>
       </div>
-
-  <!--   Core JS Files   -->
-  <script src="../../../assets/js/core/popper.min.js"></script>
-  <script src="../../../assets/js/core/bootstrap.min.js"></script>
-  <script src="../../../assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="../../../assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script src="../../../assets/js/plugins/datatables.js"></script>
-  <script>
-    if (document.getElementById('products-list')) {
-      const dataTableSearch = new simpleDatatables.DataTable("#products-list", {
-        searchable: true,
-        fixedHeight: false,
-        perPage: 7
-      });
-
-      document.querySelectorAll(".export").forEach(function(el) {
-        el.addEventListener("click", function(e) {
-          var type = el.dataset.type;
-
-          var data = {
-            type: type,
-            filename: "soft-ui-" + type,
-          };
-
-          if (type === "csv") {
-            data.columnDelimiter = "|";
-          }
-
-          dataTableSearch.export(data);
-        });
-      });
-    };
-  </script>
-  <!-- Kanban scripts -->
-  <script src="../../../assets/js/plugins/dragula/dragula.min.js"></script>
-  <script src="../../../assets/js/plugins/jkanban/jkanban.js"></script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../../../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
 </body>
 
 @stop
--- resources/views/flight-status/index.blade.php
+++ resources/views/flight-status/index.blade.php
@@ -12,7 +12,7 @@
               <div class="card-body">
                 <h1 class="mb-4 ">Flight Status</h1>
                 <div class="row">
-                  <div class="col-xl-5 col-lg-6 text-center">
+                  <div class="">
                     <p>This is where the user can know the status of the flight and its position.<p>
                   </div>
                 </div>
@@ -22,7 +22,44 @@
       </div>
   </div>
 
-
+  <table class="table table-striped table table-bordered">
+    <tr>
+        <td>NAME</td>
+        <td>DRONE</td>
+        <td>USER</td>
+        <td>DATE</td>
+        <td>TIME</td>
+        <td>LOCATION / POSITION</td>
+        <td>STATUS</td>
+    </tr>
+    <tr>
+        <td>Flight_01</td>
+        <td>Drone_01</td>
+        <td>User_01</td>
+        <td>29.7.2021</td>
+        <td>10:30:05</td>
+        <td>Damansara Perdana, 47820 Petaling Jaya, Selangor</td>
+        <td>In Air</td>
+    </tr>
+    <tr>
+      <td>Flight_02</td>
+      <td>Drone_02</td>
+      <td>User_02</td>
+      <td>30.7.2021</td>
+      <td>14:28:56</td>
+      <td>Jalan Jaya Gading, 25150 Kuantan, Pahang</td>
+      <td>Scheduled</td>
+  </tr>
+    <tr>
+      <td>Flight_03</td>
+      <td>Drone_03</td>
+      <td>User_03</td>
+      <td>31.7.2021</td>
+      <td>17:12:31</td>
+      <td>Jalan Perwira, 88100 Kota Kinabalu, Sabah</td>
+      <td>Scheduled</td>
+</tr>
+</table>
   
 </body>
 @stop
